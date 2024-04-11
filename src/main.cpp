47a363582c51173b7a2741cf46f9140cb98fdf08
Samgd14 <samgd14@live.ca>
2024-04-11 10:27:18 -0400

    Initial port commit
    Ported over from a different repo, so not the first version of this code, but most up to date

---
+++ src/main.cpp
@@ -0,0 +1,330 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+#include <Servo.h>
+#include <TaskScheduler.h>
+
+int x;
+ 
+/*--------------------------- Constantes ----------------------------*/
+ 
+#define BAUD            115200    //Serial baud rate
+ 
+#define PIN_MOT1         5
+#define PIN_MOT2         6
+#define PIN_MOT3         7
+
+
+/*------------------------- Globale variables -------------------------*/
+//PPM motor outputs 
+Servo motor1;
+Servo motor2;
+Servo motor3;
+ 
+// Xbox Controller input variables
+float LeftJoystickX_    = 0;
+float LeftJoystickY_    = 0;
+float RightJoystickX_   = 0;
+float RightJoystickY_   = 0;
+float LeftTrigger_      = 0;
+float RightTrigger_     = 0;
+bool  LeftBumper_       = 0;
+bool  RightBumper_      = 0;
+bool  AButton_          = 0;
+bool  BButton_          = 0;
+bool  XButton_          = 0;
+bool  YButton_          = 0;
+bool  LeftThumb_        = 0;
+bool  RightThumb_       = 0;
+bool  LeftDpad_         = 0;
+bool  RightDpad_        = 0;
+bool  UpDpad_           = 0;
+bool  DownDpad_         = 0;
+bool  BackButton_       = 0;
+bool  StartButton_      = 0;
+float M1, M2, M3;
+bool BackButtonAct_     ;
+ 
+/*------------------------- Function prototypes -------------------------*/
+void sendMsg();
+void readMsg();
+void serialEvent();
+void desactivateMotors();
+
+void motorUpdateCall();
+void sendMsgCall();
+void readMsgCall();
+
+void manageSerialCom();
+float manageMotors(float inputUp,float inputDown, float minRange, float maxRange, float midRange);
+float manageMotors(float input, float minRange, float maxRange, float midRange);
+
+//Task shenanigans
+Task MotorUpdateTask(50, TASK_FOREVER, &motorUpdateCall);
+Task SendMsgTask(50, TASK_FOREVER, &sendMsgCall);
+Task ReadMsgTask(0, TASK_FOREVER, &readMsgCall);
+
+Scheduler Runner;
+ 
+/*------------------------- Main function -------------------------*/
+ 
+void setup() {
+    //Adding and enabling tasks
+    Runner.addTask(ReadMsgTask);
+    Runner.addTask(SendMsgTask);
+    SendMsgTask.enable();
+    Runner.addTask(MotorUpdateTask);
+    MotorUpdateTask.enable();
+
+    //Initializing motors
+    motor1.attach(PIN_MOT1); //Triggers
+    motor2.attach(PIN_MOT2); //leftJoystickX_
+    motor3.attach(PIN_MOT3); //leftJoystickY_
+ 
+    Serial.begin(BAUD) ; // serial communication initialisation
+   
+    while(!Serial) {
+
+    }
+}
+ 
+void loop() {
+    Runner.execute();
+}
+
+void motorUpdateCall(){
+    //Update motors if enabled
+    if(BackButtonAct_ == true)
+    {
+        M1 = manageMotors(RightTrigger_,LeftTrigger_,1400,1600,1500);
+        motor1.writeMicroseconds(M1);
+    
+        M2 = manageMotors(LeftJoystickX_,1450,1600,1500);
+        motor2.writeMicroseconds(M2);
+    
+        M3 = manageMotors(RightJoystickY_,1400,1600,1500);
+        motor3.writeMicroseconds(M3);
+    }
+    //Set them to 0 speed if not enabled
+    else
+    {
+        motor1.writeMicroseconds(1500);
+        motor2.writeMicroseconds(1500);
+        motor2.writeMicroseconds(1500);
+    }
+}
+
+void serialEvent(){ReadMsgTask.enable();}
+
+void readMsgCall(){
+    readMsg();
+    ReadMsgTask.disable();
+}
+
+void sendMsgCall(){
+    sendMsg();
+}
+ 
+/*------------------- Function definitions ----------------------*/
+
+//Motor command calculation (2 inputs)
+float manageMotors(float inputUp, float inputDown, float minRange, float maxRange, float midRange){
+
+    //Calculates an overall input then uses the other function
+    float input = inputUp-inputDown;
+    float range = manageMotors(input, minRange, maxRange, midRange);
+ 
+    return range;
+}
+ 
+//Motor command calculation (2 inputs)
+float manageMotors(float input, float minRange, float maxRange, float midRange){
+
+    float range;
+ 
+    if(input>=0)
+    {
+        range = ((maxRange-midRange) * input) + midRange ;
+    }else if(input<0){
+        range = midRange+((midRange-minRange) * input);
+    }
+ 
+    return range;
+}
+
+//Send the JSON Message throught the serial port
+void sendMsg(){
+    JsonDocument doc;
+
+    // Message Elements
+    doc["LeftJoystickX"]    = LeftJoystickX_    ;
+    //doc["LeftJoystickY"]    = LeftJoystickY_  ;
+    //doc["RightJoystickX_"]  = RightJoystickX_ ;
+    doc["RightJoystickY_"]  = RightJoystickY_   ;
+    doc["LeftTrigger"]      = LeftTrigger_      ;
+    doc["RightTrigger"]     = RightTrigger_     ;  
+    /*
+    doc["LeftBumper"]       = LeftBumper_       ;
+    doc["RightBumper"]      = RightBumper_      ;
+   
+    doc["AButton"]          = AButton_          ;
+    doc["BButton"]          = BButton_          ;
+    doc["XButton"]          = XButton_          ;
+    doc["YButton"]          = YButton_          ;
+ 
+    doc["LeftThumb"]        = LeftThumb_        ;
+    doc["RightThumb"]       = RightThumb_       ;
+    doc["LeftDpad"]         = LeftDpad_         ;
+    doc["RightDpad"]        = RightDpad_        ;
+    doc["UpDpad"]           = UpDpad_           ;
+    doc["DownDpad"]         = DownDpad_         ;
+    */
+    doc["BackButton"]       = BackButton_       ;
+ 
+    //doc["StartButton"]      = StartButton_      ;
+ 
+    doc["Moteur 1"]       = M1       ;
+    doc["Moteur 2"]       = M2       ;
+    doc["Moteur 3"]       = M3       ;
+    doc["BackButtonAct"]  = BackButtonAct_      ;
+   
+ 
+    //Serialization
+    serializeJson(doc, Serial);
+ 
+    //Sending JSON over serial
+    Serial.println(); 
+}
+
+//Message decoding function
+//Gets called on serial events
+void readMsg(){
+    // Reading Json message
+    JsonDocument doc;
+    JsonVariant parse_msg;
+ 
+    // Reading Serial port
+    DeserializationError error = deserializeJson(doc, Serial);
+ 
+    // If Error in message
+    if(error) {
+        Serial.print("deserialize() failed:");
+        Serial.println(error.c_str());
+        return;
+    }
+ 
+    // Message analysis
+ 
+    parse_msg = doc["LeftJoystickX"];
+    if(!parse_msg.isNull()){
+        LeftJoystickX_ = doc["LeftJoystickX"].as<float>();
+    }
+    /*
+    parse_msg = doc["LeftJoystickY"];
+    if(!parse_msg.isNull()){
+        LeftJoystickY_ = doc["LeftJoystickY"].as<float>();
+    }
+ 
+    parse_msg = doc["RightJoystickX"];
+    if(!parse_msg.isNull()){
+        RightJoystickX_ = doc["RightJoystickX"].as<float>();
+    }
+    */
+    parse_msg = doc["RightJoystickY"];
+    if(!parse_msg.isNull()){
+        RightJoystickY_ = doc["RightJoystickY"].as<float>();
+    }
+ 
+    parse_msg = doc["LeftTrigger"];
+    if(!parse_msg.isNull()){
+        LeftTrigger_ = doc["LeftTrigger"].as<float>();
+    }
+   
+    parse_msg = doc["RightTrigger"];
+    if(!parse_msg.isNull()){
+        RightTrigger_ = doc["RightTrigger"].as<float>();
+    }
+
+    /*
+    parse_msg = doc["LeftBumper"];
+    if(!parse_msg.isNull()){
+        LeftBumper_ = doc["LeftBumper"].as<bool>();
+    }
+ 
+    parse_msg = doc["RightBumper"];
+    if(!parse_msg.isNull()){
+        RightBumper_ = doc["RightBumper"].as<bool>();
+    }
+ 
+   
+    parse_msg = doc["AButton"];
+    if(!parse_msg.isNull()){
+        AButton_ = doc["AButton"].as<bool>();
+    }
+ 
+    parse_msg = doc["BButton"];
+    if(!parse_msg.isNull()){
+        BButton_ = doc["BButton"].as<bool>();
+    }
+ 
+    parse_msg = doc["XButton"];
+    if(!parse_msg.isNull()){
+        XButton_ = doc["XButton"].as<bool>();
+    }
+ 
+    parse_msg = doc["YButton"];
+    if(!parse_msg.isNull()){
+        YButton_ = doc["YButton"].as<bool>();
+    }
+   
+    parse_msg = doc["LeftThumb"];
+    if(!parse_msg.isNull()){
+        LeftThumb_ = doc["LeftThumb"].as<bool>();
+    }
+ 
+    parse_msg = doc["RightThumb"];
+    if(!parse_msg.isNull()){
+        RightThumb_ = doc["RightThumb"].as<bool>();
+    }
+ 
+    parse_msg = doc["LeftDpad"];
+    if(!parse_msg.isNull()){
+        LeftDpad_ = doc["LeftDpad"].as<bool>();
+    }
+ 
+    parse_msg = doc["RightDpad"];
+    if(!parse_msg.isNull()){
+        RightDpad_ = doc["RightDpad"].as<bool>();
+    }
+ 
+    parse_msg = doc["UpDpad"];
+    if(!parse_msg.isNull()){
+        UpDpad_ = doc["UpDpad"].as<bool>();
+    }
+ 
+    parse_msg = doc["DownDpad"];
+    if(!parse_msg.isNull()){
+        DownDpad_ = doc["DownDpad"].as<bool>();
+    }
+    */
+    
+    parse_msg = doc["BackButton"];
+    if(!parse_msg.isNull()){
+        BackButton_ = doc["BackButton"].as<bool>();
+    }
+   
+   /*
+    parse_msg = doc["StartButton"];
+    if(!parse_msg.isNull()){
+        StartButton_ = doc["StartButton"].as<bool>();
+    }
+   */
+    parse_msg = doc["BackButtonAct"];
+    if(!parse_msg.isNull()){
+        BackButtonAct_ = doc["BackButtonAct"].as<bool>();
+    }
+ 
+   
+}
+ 
+//===============
